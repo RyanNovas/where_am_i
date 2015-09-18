cebc62ef806eb6264a0464c3cb255cb7fe470549
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2015-09-18 17:53:23 -0400

    get information into params

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,2 +1,11 @@
+require "pry"
 class NeighborhoodsController < ApplicationController
+
+  def index
+    longitude = params["/neighborhood"][:latitude]
+    latitude = params["/neighborhood"][:longitude]
+    respond_to do |format|
+      format.js{}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :vote_counts
   resources :neighborhoods
   resources :users
+
+  post "/" => "neighborhoods#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
