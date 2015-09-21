b3fe2aa11ec46e7ef6c7decabdb8dd51e00e2381
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2015-09-21 10:13:06 -0400

    form submits to rails automatically

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -2,8 +2,9 @@ require "pry"
 class NeighborhoodsController < ApplicationController
 
   def index
-    longitude = params["/neighborhood"][:latitude]
-    latitude = params["/neighborhood"][:longitude]
+    binding.pry
+    longitude = params["/neighborhoods"][:latitude]
+    latitude = params["/neighborhoods"][:longitude]
     respond_to do |format|
       format.js{}
     end
