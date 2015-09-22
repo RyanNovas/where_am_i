ab15c6a168f55d6cb8ce1299eac1ca7db787f3ec
RyanNovas <ryan@quill.org>
2015-09-22 18:35:19 -0400

    adds self as an argument

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,7 +2,8 @@ class Neighborhood < ActiveRecord::Base
   has_many :sessions
   has_one :vote_count
 
-  def self.find_neighborhood_name(latitude, longitude)
+  def self.find_neighborhood_name(session_instance, latitude, longitude)
+    @session = session_instance
     @url = "http://api.nytimes.com/svc/politics/v2/districts.json?lat=#{latitude}&lng=#{longitude}&api-key=#{ENV["Neighborhood_key"]}"
     @response = HTTParty.get(@url)
     while @response.code != 200
@@ -20,12 +21,12 @@ class Neighborhood < ActiveRecord::Base
 
   def self.neighborhood_exists?
     @neighborhood = Neighborhood.all.find_by(name: @neighborhood_name)
-    @neighborhood ? Session.all.last.set_neighborhood_id(@neighborhood.id) : create_neighborhood
+    @neighborhood ? @session.set_neighborhood_id(@neighborhood.id) : create_neighborhood
   end
 
   def self.create_neighborhood
     @neighborhood = Neighborhood.create(name: @neighborhood_name, borough: @borough_name)
-    Session.all.last.set_neighborhood_id(@neighborhood.id)
+    @session.set_neighborhood_id(@neighborhood.id)
   end
 
 
