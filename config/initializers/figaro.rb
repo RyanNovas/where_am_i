f5a2a617a9b3e1ce2af630a6595e70d6142d6b8a
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2015-09-22 12:11:03 -0400

    require devs to use GoogleMaps_key

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,2 +1,3 @@
-Figaro.require_keys("Neighborhood_key")
+Figaro.require_keys("Neighborhood_key", "GoogleMaps_key")
 Neighborhood_key = ENV["Neighborhood_key"]
+GoogleMaps_key = ENV["GoogleMaps_key"]
