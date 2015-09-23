38903e115172d91530c43cf8ca79cbb3963fe4f3
RyanNovas <ryan@quill.org>
2015-09-23 15:01:16 -0400

    works

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,20 +1,23 @@
 class VotesController < ApplicationController
 
+
+
+
   def index
     @votes = Votes.all.last
   end
 
   def up_vote
-    vote = Votes.all.last
-    vote.up_votes +=1
-    vote.save
+    @vote = Votes.all.find_by(neighborhood_id: params["neighborhood_id"])
+    @vote.up_votes +=1
+    @vote.save
     redirect_to root_path
   end
 
   def down_vote
-    vote = Votes.all.last
-    vote.down_votes -=1
-    vote.save
+    @vote = Votes.all.find_by(neighborhood_id: params["neighborhood_id"])
+    @vote.down_votes +=1
+    @vote.save
     redirect_to root_path
   end
 
