94c51bb4f14b2bb11d5d07a4bc48a26112932313
NancyKo <konancys@gmail.com>
2015-09-23 14:06:45 -0400

    Add StreetEasy avg one bd rental price

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
     initial_session = Session.create(session_params)
     @session = initial_session.look_up_neighborhood
     yelp
+    avg_one_bdrm
     render :show
   end
 
@@ -20,5 +21,11 @@ class SessionsController < ApplicationController
   def yelp
     @bar = yelp_attributes(session_params["latitude"], session_params["longitude"], "bars")
     @restaurant = yelp_attributes(session_params["latitude"], session_params["longitude"], "restaurants")
+  end
+
+  def avg_one_bdrm
+    rentals_end_point = "http://streeteasy.com/nyc/api/rentals/data?criteria=area:#{@session.neighborhood['name']}-#{@session.neighborhood['borough']}%7Cbeds:1&key=#{ENV["StreetEasy_key"]}&format=json"
+    get_req = HTTParty.get(rentals_end_point)
+    @price = get_req["average_price"]
   end
 end
