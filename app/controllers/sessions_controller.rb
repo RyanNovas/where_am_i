46443a589911f0e792dec6a5b36dbb5a0e3638ae
NancyKo <konancys@gmail.com>
2015-09-23 15:13:20 -0400

    Delete bathroom and create StreetEasy module

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   include YelpApi
+  include StreetEasyApi
 
   def index
     @session = Session.new
@@ -14,18 +15,13 @@ class SessionsController < ApplicationController
   end
 
   private
-
   def session_params
     params.require(:session).permit(:latitude, :longitude)
   end
+
   def yelp
     @bar = yelp_attributes(session_params["latitude"], session_params["longitude"], "bars")
     @restaurant = yelp_attributes(session_params["latitude"], session_params["longitude"], "restaurants")
   end
 
-  def avg_one_bdrm
-    rentals_end_point = "http://streeteasy.com/nyc/api/rentals/data?criteria=area:#{@session.neighborhood['name']}-#{@session.neighborhood['borough']}%7Cbeds:1&key=#{ENV["StreetEasy_key"]}&format=json"
-    get_req = HTTParty.get(rentals_end_point)
-    @price = get_req["average_price"]
-  end
 end
